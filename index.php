f4a0a04c478a30f814b364eedc6ba3f2417caba6
justucru <56866258+justucru@users.noreply.github.com>
2020-03-18 14:57:57 +0100

    Add files

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Justucru" => [
+    "city" => "Lyon",
+    "weapon" => "batte"
+    ]
+];
+
+var_dump($characters);
